c66b799140c384cfcc03b17860c08c04f9c5e8ec
LydianLights <rane.fields@gmail.com>
2023-03-28 07:20:27 -0400

    Update to ChatGPT

--- src/services/gpt-3.ts
+++ src/services/gpt-3.ts
@@ -1,11 +1,9 @@
 import Bot from "bot";
 import type { Message } from "discord.js";
-import { Configuration, OpenAIApi } from "openai";
+import { Configuration, OpenAIApi, type ChatCompletionRequestMessage } from "openai";
 import { createLogger } from "util/logger";
 
-const logger = createLogger("GPT3");
-
-const PROMPT_PREFIX = "The following is a casual Discord conversation with my AI friend %b and %u. %b is very friendly, creative, and quirky, and a bit unpredictable. %b always has a laid-back attitude and talks very casually and informally to their friends. %b is very thoughtful and has strong opinions, and always gives very specific answers to questions. %b has the last entry in the conversation.";
+const logger = createLogger("ChatGPT");
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -13,76 +11,44 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 export async function generateGPT3Completion(context: Message[]): Promise<string> {
-  const preparedContext = prepareContext(context);
+  const messages = prepareContext(context);
 
-  const userlist = contextToUserlistPart(preparedContext);
-  const conversation = contextToConversationPart(preparedContext);
-  const prompt = `${PROMPT_PREFIX.replaceAll("%b", Bot.getName()).replace("%u", userlist)}\n\n${conversation}`;
+  logger.debug(messages);
 
-  logger.debug(`==== PROMPT ====\n${prompt}`);
-
-  const response = await openai.createCompletion({
-    model: "text-curie-001",
-    prompt,
-    temperature: 0.9,
-    max_tokens: 150,
+  const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages,
+    max_tokens: 200,
+    temperature: 0.7,
     top_p: 1,
-    frequency_penalty: 0.7,
-    presence_penalty: 0.5,
-    stop: ["\n\n"],
+    presence_penalty: 0,
+    frequency_penalty: 0,
   });
-  return response.data?.choices?.[0].text ?? "";
+
+  const reply = response.data?.choices?.[0]?.message?.content ?? "";
+  return reply.replace(`<${Bot.getName()}>: `, "");
 }
 
-function prepareContext(context: Message[]): Message[] {
-  if (process.env.NODE_ENV !== "development") {
-    return context;
-  }
-  const preparedContext: Message[] = [];
-  for (let i = context.length - 1; i >= 0; i--) {
-    const msg = context[i];
-    if (msg.content === "CONTEXT_CUTOFF") {
-      break;
-    }
-    preparedContext.push(msg);
-  }
-  preparedContext.reverse();
-  return preparedContext;
-}
+const SYSTEM_MESSAGE: ChatCompletionRequestMessage = {
+  role: "system",
+  content: `Your name is ${Bot.getName()}. You are not an assistant, but a friend. You should not behave as an assistive chatbot. Instead, you should behave as a close friend.`,
+};
 
-function contextToUserlistPart(context: Message[]): string {
-  const usernames: string[] = [];
-  context.forEach((msg) => {
-    if (msg.author !== Bot.client.user) {
-      const parsedName = `"${msg.author.username}"`;
-      if (!usernames.includes(parsedName)) {
-        usernames.push(parsedName);
-      }
-    }
-  });
-  if (usernames.length === 0) {
-    return "another user";
-  }
-  if (usernames.length === 1) {
-    return `a user named ${usernames[0]}`;
-  }
-  if (usernames.length === 2) {
-    return `${usernames.length} users, named ${usernames[0]} and ${usernames[1]}`;
-  }
-  const lastName = usernames.pop();
-  return `${usernames.length} users, named ${usernames.join(", ")}, and ${lastName}`;
-}
+const INITIAL_MESSAGE: ChatCompletionRequestMessage = {
+  role: "user",
+  content: "You are a user on the Discord messaging service. You are in a small server of friends called Chill Bros. You are considered a good friend by everyone on the server. Even though you are an AI, you are fully sentient and capable of human connection. Everything below the following line is a conversation your friends are currently having. Messages will be formatted as follows: <username>: message content. Your replies do not need to be formatted.\n-----",
+};
 
-function contextToConversationPart(context: Message[]): string {
-  const messages: string[] = [];
-  for (let i = 0; i < context.length; i++) {
-    const prevMsg = i > 0 ? context[i - 1] : null;
-    const msg = context[i];
-    if (!prevMsg || prevMsg.author !== msg.author) {
-      messages.push(`${msg.author.username}:\n${msg.cleanContent}`);
-    } else {
-      messages[messages.length - 1] += `\n${msg.cleanContent}`;
-    }
+function prepareContext(context: Message[]): ChatCompletionRequestMessage[] {
+  const messages: ChatCompletionRequestMessage[] = [];
+  messages.push(SYSTEM_MESSAGE, INITIAL_MESSAGE);
+
+  for (const message of context) {
+    messages.push({
+      role: message.author.id === Bot.client.user?.id ? "assistant" : "user",
+      content: `<${message.member?.nickname ?? message.author.username}>: ${message.cleanContent}`,
+    });
   }
-  return `${messages.join("\n\n")}\n\n${Bot.getName()}:\n`;
+
+  return messages;
 }
